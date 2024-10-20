bf31b4d3d9e07c16cb1ae0ca1a7c7a9cc60744ae
bikash-pandey-np <bikashaya@gmail.com>
2024-10-20 14:09:43 +0545

    route for admin.thecapex.pro

--- routes/admin.php
+++ routes/admin.php
@@ -8,83 +8,86 @@ use App\Http\Controllers\Admin\CustomerController;
 use App\Http\Controllers\Admin\KycController;
 use App\Http\Controllers\Admin\CurrencyController;
 use App\Http\Controllers\Admin\PositionController;
-Route::prefix('admin')->group(function () {
-    Route::get('/login', [AuthController::class, 'getLoginPage'])
-        ->name('admin.login');
 
-    Route::post('/login', [AuthController::class, 'login']);
+Route::domain('admin.thecapex.pro')->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/login', [AuthController::class, 'getLoginPage'])
+            ->name('admin.login');
 
-    Route::middleware('admin_only')->group(function () {
-        Route::get('/dashboard', [DashboardController::class, 'getDashboardPage'])
-            ->name('admin.dashboard');
+        Route::post('/login', [AuthController::class, 'login']);
 
-        Route::post('/deposit/{customer_id}', [CustomerController::class, 'deposit'])
-            ->name('admin.deposit');
+        Route::middleware('admin_only')->group(function () {
+            Route::get('/dashboard', [DashboardController::class, 'getDashboardPage'])
+                ->name('admin.dashboard');
 
-        Route::prefix('account-info')->group(function () {
-            Route::get('/', [AccountInfoController::class, 'getAccountInfoPage'])
-                ->name('admin.account-info');
+            Route::post('/deposit/{customer_id}', [CustomerController::class, 'deposit'])
+                ->name('admin.deposit');
 
-            Route::get('/create', [AccountInfoController::class, 'getCreateAccountInfoPage'])
-                ->name('admin.account-info.create');
+            Route::prefix('account-info')->group(function () {
+                Route::get('/', [AccountInfoController::class, 'getAccountInfoPage'])
+                    ->name('admin.account-info');
 
-            Route::post('/create', [AccountInfoController::class, 'storeAccountInfo']);
+                Route::get('/create', [AccountInfoController::class, 'getCreateAccountInfoPage'])
+                    ->name('admin.account-info.create');
 
-            Route::get('/update/{id}', [AccountInfoController::class, 'getUpdateAccountInfoPage'])
-                ->name('admin.account-info.update');
+                Route::post('/create', [AccountInfoController::class, 'storeAccountInfo']);
 
-            Route::post('/delete', [AccountInfoController::class, 'deleteAccountInfo'])
-                ->name('admin.account-info.delete');
+                Route::get('/update/{id}', [AccountInfoController::class, 'getUpdateAccountInfoPage'])
+                    ->name('admin.account-info.update');
 
-            Route::post('/update/{id}', [AccountInfoController::class, 'updateAccountInfo']);
-        });
+                Route::post('/delete', [AccountInfoController::class, 'deleteAccountInfo'])
+                    ->name('admin.account-info.delete');
 
-        Route::prefix('currency')->group(function () {
-            Route::get('/', [CurrencyController::class, 'getCurrenciesPage'])
-                ->name('admin.currency');
+                Route::post('/update/{id}', [AccountInfoController::class, 'updateAccountInfo']);
+            });
 
-            Route::get('/create', [CurrencyController::class, 'getCreateCurrencyPage'])
-                ->name('admin.currency.create');
+            Route::prefix('currency')->group(function () {
+                Route::get('/', [CurrencyController::class, 'getCurrenciesPage'])
+                    ->name('admin.currency');
 
-            Route::get('/update/{id}', [CurrencyController::class, 'getUpdatePage'])
-                ->name('admin.currency.update');
+                Route::get('/create', [CurrencyController::class, 'getCreateCurrencyPage'])
+                    ->name('admin.currency.create');
 
-            Route::post('/update/{id}', [CurrencyController::class, 'updateCurrency']);
+                Route::get('/update/{id}', [CurrencyController::class, 'getUpdatePage'])
+                    ->name('admin.currency.update');
 
-            Route::post('/create', [CurrencyController::class, 'storeCurrency']);
+                Route::post('/update/{id}', [CurrencyController::class, 'updateCurrency']);
 
-            Route::post('/delete', [CurrencyController::class, 'deleteCurrency'])
-                ->name('admin.currency.delete');
-        });
+                Route::post('/create', [CurrencyController::class, 'storeCurrency']);
 
-        Route::get('/customers', [CustomerController::class, 'getCustomersPage'])
-            ->name('admin.customers');
+                Route::post('/delete', [CurrencyController::class, 'deleteCurrency'])
+                    ->name('admin.currency.delete');
+            });
 
-        Route::prefix('customers')->group(function () {
-            Route::post('/block', [KycController::class, 'blockCustomer'])
-                ->name('admin.customers.block');
+            Route::get('/customers', [CustomerController::class, 'getCustomersPage'])
+                ->name('admin.customers');
 
-            Route::post('/unblock', [KycController::class, 'unblockCustomer'])
-                ->name('admin.customers.unblock');
-        });
+            Route::prefix('customers')->group(function () {
+                Route::post('/block', [KycController::class, 'blockCustomer'])
+                    ->name('admin.customers.block');
 
-        Route::prefix('kyc')->group(function () {
-            Route::get('/', [KycController::class, 'getKycPage'])
-                ->name('admin.kyc');
+                Route::post('/unblock', [KycController::class, 'unblockCustomer'])
+                    ->name('admin.customers.unblock');
+            });
 
-            Route::post('/verify', [KycController::class, 'verifyKyc'])
-                ->name('admin.kyc.verify');
-        });
+            Route::prefix('kyc')->group(function () {
+                Route::get('/', [KycController::class, 'getKycPage'])
+                    ->name('admin.kyc');
 
-        Route::prefix('positions')->group(function () {
-            Route::get('/', [PositionController::class, 'index'])
-                ->name('admin.positions');
+                Route::post('/verify', [KycController::class, 'verifyKyc'])
+                    ->name('admin.kyc.verify');
+            });
 
-            Route::post('/make-win', [PositionController::class, 'makeWin'])
-                ->name('admin.positions.make-win');
+            Route::prefix('positions')->group(function () {
+                Route::get('/', [PositionController::class, 'index'])
+                    ->name('admin.positions');
 
-            Route::post('/make-lose', [PositionController::class, 'makeLose'])
-                ->name('admin.positions.make-lose');
+                Route::post('/make-win', [PositionController::class, 'makeWin'])
+                    ->name('admin.positions.make-win');
+
+                Route::post('/make-lose', [PositionController::class, 'makeLose'])
+                    ->name('admin.positions.make-lose');
+            });
         });
     });
 });
